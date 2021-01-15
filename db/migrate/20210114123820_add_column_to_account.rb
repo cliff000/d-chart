dd1430d9b27d334d1e6d696e97b76003824f9dd4
cliff000 <cliff.5963@gmail.com>
2021-01-15 13:14:58 +0900

    マイグレーションファイルの削除

--- db/migrate/20210114123820_add_column_to_account.rb
+++
@@ -1,8 +0,0 @@
-class AddColumnToAccount < ActiveRecord::Migration[5.2]
-  def change
-    add_column  :account,  :confirmation_token,  :string
-    add_column  :account,  :confirmed_at,        :datetime
-    add_column  :account,  :confirmation_sent_at,:datetime    
-    add_column  :account,  :unconfirmed_email,   :string       #email変更時の認証が不要であれば、この項目は必要ではありません。ただし、configの「reconfirmable」を「false」にする必要があります。
-  end
-end

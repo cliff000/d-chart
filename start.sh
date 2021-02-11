b57eb00f27bf73d46db8f20f5e374f6b08f192a7
cliff000 <cliff.5963@gmail.com>
2021-02-11 18:05:27 +0900

    KC2021Feb用のデータ追加 その他いろいろ

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+RAILS_PORT=3000
+if [ -n "$PORT" ]; then
+    RAILS_PORT=$PORT
+fi
+
+# Remove a potentially pre-existing server.pid for Rails.
+rm -f tmp/pids/server.pid
+
+bin/rails s -p $RAILS_PORT -b 0.0.0.0

fac65b1343865084a6d3d05496c5e92cc30eadf4
cliff000 <cliff.5963@gmail.com>
2021-04-15 18:21:22 +0900

    production.rbの記述を変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #device
-  config.action_mailer.default_url_options = { host: 'kcchart.com' }
+  config.action_mailer.default_url_options = { host: 'htttps://kcchart.com' }
 
     #mail
     config.action_mailer.delivery_method = :smtp
